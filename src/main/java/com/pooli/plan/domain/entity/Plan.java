c6353ada6e18171ae233775231ede5d3c0c51ee1
ms-0o0 <201900703@hufs.ac.kr>
2026-02-23 16:38:36 +0900

    feat(plan): PRIVATE로 생성자 접근 변경

--- src/main/java/com/pooli/plan/domain/entity/Plan.java
+++ src/main/java/com/pooli/plan/domain/entity/Plan.java
@@ -8,7 +8,7 @@ import java.time.LocalDateTime;
 @Getter
 @Builder
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Plan {
 
     private Integer planId;
